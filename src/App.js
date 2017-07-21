7e3947ba196e30bab67a0e23afae8c9eb3e1b36d
bmaupin <bmaupin@users.noreply.github.com>
2017-07-21 14:21:55 -0400

    Clean up a couple autogenerated files

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
@@ -7,7 +7,7 @@ class App extends Component {
     return (
       <div className="App">
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+          {/*<img src={logo} className="App-logo" alt="logo" />*/}
           <h2>Welcome to React</h2>
         </div>
         <p className="App-intro">
