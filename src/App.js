f3ac68e40061ea346fdc98126a206be72ec3ede8
bmaupin <bmaupin@users.noreply.github.com>
2017-07-28 15:10:06 -0400

    Add app bar

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import AppBar from 'material-ui/AppBar';
 import Checkbox from 'material-ui/Checkbox';
 import Civ5Save from 'civ5save';
 import { createMuiTheme } from 'material-ui/styles';
@@ -5,10 +6,14 @@ import createPalette from 'material-ui/styles/palette';
 import { FormGroup, FormControlLabel } from 'material-ui/Form';
 import Grid from 'material-ui/Grid';
 import Icon from 'material-ui/Icon';
+import IconButton from 'material-ui/IconButton';
 import List, { ListItem, ListItemIcon, ListItemText } from 'material-ui/List';
 import { MuiThemeProvider } from 'material-ui/styles';
 import React, { Component } from 'react';
+import SvgIcon from 'material-ui/SvgIcon';
 import TextField from 'material-ui/TextField';
+import Toolbar from 'material-ui/Toolbar';
+import Typography from 'material-ui/Typography';
 import './App.css';
 
 const darkTheme = createMuiTheme({
@@ -46,37 +51,60 @@ class App extends Component {
   render() {
     return (
       <MuiThemeProvider className="App" theme={darkTheme}>
-        <Grid container
-          style={{
-            background: darkTheme.palette.background.default,
-            color: darkTheme.palette.text.primary,
-          }}>
-          <Grid item>
-            <List style={{
-              backgroundColor: darkTheme.palette.background.paper,
-              // TODO: height is bigger than the viewable area
-              height: '100vh'
+        <div>
+          <AppBar position="static">
+            <Toolbar>
+              <Typography type="title"
+                // This makes sure the button is aligned on the right
+                style={{ flex:"1" }}>
+                {/* TODO: make this a constant */}
+                Civilization V save editor
+              </Typography>
+              {/* TODO: make the URL a constant */}
+              <IconButton color="contrast" href="https://github.com/bmaupin/react-civ5save">
+                <SvgIcon>
+                  <path d="M12.007 0C6.12 0 1.1 4.27.157 10.08c-.944 5.813 2.468 11.45 8.054 13.312.19.064.397.033.555-.084.16-.117.25-.304.244-.5v-2.042c-3.33.735-4.037-1.56-4.037-1.56-.22-.726-.694-1.35-1.334-1.756-1.096-.75.074-.735.074-.735.773.103 1.454.557 1.846 1.23.694 1.21 2.23 1.638 3.45.96.056-.61.327-1.178.766-1.605-2.67-.3-5.462-1.335-5.462-6.002-.02-1.193.42-2.35 1.23-3.226-.327-1.015-.27-2.116.166-3.09 0 0 1.006-.33 3.3 1.23 1.966-.538 4.04-.538 6.003 0 2.295-1.5 3.3-1.23 3.3-1.23.445 1.006.49 2.144.12 3.18.81.877 1.25 2.033 1.23 3.226 0 4.607-2.805 5.627-5.476 5.927.578.583.88 1.386.825 2.206v3.29c-.005.2.092.393.26.507.164.115.377.14.565.063 5.568-1.88 8.956-7.514 8.007-13.313C22.892 4.267 17.884.007 12.008 0z" />
+                </SvgIcon>
+              </IconButton>
+            </Toolbar>
+          </AppBar>
+          <Grid container
+            // Without this, padding is added to every grid item
+            gutter={0}
+            style={{
+              background: darkTheme.palette.background.default,
+              color: darkTheme.palette.text.primary,
+              // Fixes excess height and app bar drop shadow
+              position: "fixed",
+              // Fixes app bar drop shadow (together with position=fixed)
+              zIndex: "-1",
             }}>
-              <FileUploader
-                onNewSavegame={this.handleNewSavegame}
-              />
-              <FileDownloader
+            <Grid item>
+              <List style={{
+                backgroundColor: darkTheme.palette.background.paper,
+                height: '100vh'
+              }}>
+                <FileUploader
+                  onNewSavegame={this.handleNewSavegame}
+                />
+                <FileDownloader
+                  savegame={this.state.savegame}
+                />
+              </List>
+            </Grid>
+            <Grid item>
+              <GeneralProperties
                 savegame={this.state.savegame}
               />
-            </List>
+            </Grid>
+            <Grid item>
+              <SavePropertiesList
+                onPropertyChanged={this.handlePropertyChange}
+                savegame={this.state.savegame}
+              />
+            </Grid>
           </Grid>
-          <Grid item>
-            <GeneralProperties
-              savegame={this.state.savegame}
-            />
-          </Grid>
-          <Grid item>
-            <SavePropertiesList
-              onPropertyChanged={this.handlePropertyChange}
-              savegame={this.state.savegame}
-            />
-          </Grid>
-        </Grid>
+        </div>
       </MuiThemeProvider>
     );
   }
