0e70ef7725ff4210477b27e822c39a4d3b04d35c
bmaupin <bmaupin@users.noreply.github.com>
2017-08-21 14:50:44 -0400

    Add bug report and demo buttons

--- src/App.js
+++ src/App.js
@@ -22,6 +22,8 @@ import Typography from 'material-ui/Typography';
 import { createStyleSheet, withStyles } from 'material-ui/styles';
 import './App.css';
 
+const REPO_URL = 'https://github.com/bmaupin/react-civ5save';
+
 const darkTheme = (() => {
   const palette = createPalette({
     type: 'dark',
@@ -112,8 +114,7 @@ class App extends Component {
                 {/* TODO: make this a constant */}
                 Civilization V save editor
               </Typography>
-              {/* TODO: make the URL a constant */}
-              <IconButton color="contrast" href="https://github.com/bmaupin/react-civ5save">
+              <IconButton color="contrast" href={REPO_URL}>
                 <SvgIcon>
                   <path d="M12.007 0C6.12 0 1.1 4.27.157 10.08c-.944 5.813 2.468 11.45 8.054 13.312.19.064.397.033.555-.084.16-.117.25-.304.244-.5v-2.042c-3.33.735-4.037-1.56-4.037-1.56-.22-.726-.694-1.35-1.334-1.756-1.096-.75.074-.735.074-.735.773.103 1.454.557 1.846 1.23.694 1.21 2.23 1.638 3.45.96.056-.61.327-1.178.766-1.605-2.67-.3-5.462-1.335-5.462-6.002-.02-1.193.42-2.35 1.23-3.226-.327-1.015-.27-2.116.166-3.09 0 0 1.006-.33 3.3 1.23 1.966-.538 4.04-.538 6.003 0 2.295-1.5 3.3-1.23 3.3-1.23.445 1.006.49 2.144.12 3.18.81.877 1.25 2.033 1.23 3.226 0 4.607-2.805 5.627-5.476 5.927.578.583.88 1.386.825 2.206v3.29c-.005.2.092.393.26.507.164.115.377.14.565.063 5.568-1.88 8.956-7.514 8.007-13.313C22.892 4.267 17.884.007 12.008 0z" />
                 </SvgIcon>
@@ -146,6 +147,16 @@ class App extends Component {
                   disabled={this.state.savegameState !== App.SAVEGAME_STATES.LOADED}
                   savegame={this.state.savegame}
                 />
+                <ListItem button component="a" href={`${REPO_URL}/issues`}>
+                  <ListItemIcon>
+                    <Icon>bug_report</Icon>
+                  </ListItemIcon>
+                  <ListItemText primary="Report bug" />
+                </ListItem>
+                <DemoButton
+                  changeSavegameState={this.changeSavegameState}
+                  onNewSavegame={this.handleNewSavegame}
+                />
               </List>
             </div>
             <div
@@ -162,6 +173,12 @@ class App extends Component {
                   </ListItem>
                   <ListItem>
                     <ListItemText primary="← Download new save file with changes" />
+                  </ListItem>
+                  <ListItem>
+                    <ListItemText primary="← Report a bug" />
+                  </ListItem>
+                  <ListItem>
+                    <ListItemText primary="← Demo with a real save file" />
                   </ListItem>
                 </List>
               }
@@ -234,6 +251,49 @@ class App extends Component {
           </div>
         </div>
       </MuiThemeProvider>
+    );
+  }
+}
+
+class DemoButton extends Component {
+  constructor(props) {
+    super(props);
+
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  getFileBlob(url) {
+    return new Promise(function (resolve, reject) {
+      let xhr = new XMLHttpRequest();
+      xhr.open('GET', url);
+      xhr.responseType = 'blob';
+      xhr.addEventListener('load', function() {
+        resolve(xhr.response);
+      });
+      xhr.addEventListener('error', function() {
+        reject(xhr.statusText);
+      });
+      xhr.send();
+    });
+  }
+
+  async handleClick(event) {
+    this.props.changeSavegameState(App.SAVEGAME_STATES.LOADING);
+    let fileBlob = await this.getFileBlob('demo/demo.Civ5Save');
+    let demoSavegame = await Civ5Save.fromFile(fileBlob);
+    this.props.onNewSavegame(demoSavegame);
+  }
+
+  render() {
+    return (
+      <ListItem button
+        onClick={this.handleClick}
+      >
+        <ListItemIcon>
+          <Icon>play_circle_outline</Icon>
+        </ListItemIcon>
+        <ListItemText primary="Demo" />
+      </ListItem>
     );
   }
 }
