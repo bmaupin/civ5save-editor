907e718f847592e47a7b2e5c2d3411aa1c7aa7e5
bmaupin <bmaupin@users.noreply.github.com>
2020-01-14 14:06:40 -0500

    Updates

---
+++ precache-manifest.40146abcc7d38651932edb1bf9af96ce.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c76d5c6482df6b56573db39d641bad0",
+    "url": "/civ5save-editor/index.html"
+  },
+  {
+    "revision": "71fe910b4ca96576f8ab",
+    "url": "/civ5save-editor/static/css/main.d8e25dda.chunk.css"
+  },
+  {
+    "revision": "fa50aa8a4be9130c3711",
+    "url": "/civ5save-editor/static/js/2.1598100c.chunk.js"
+  },
+  {
+    "revision": "5d4a2b4e2c089bcbd2034ada878a5baf",
+    "url": "/civ5save-editor/static/js/2.1598100c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "71fe910b4ca96576f8ab",
+    "url": "/civ5save-editor/static/js/main.2da8e7cd.chunk.js"
+  },
+  {
+    "revision": "0f70e5eeb2be5bddf6ee",
+    "url": "/civ5save-editor/static/js/runtime-main.ced1608b.js"
+  }
+]);
